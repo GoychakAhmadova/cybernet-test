cc80c3b53d8fdf4b84a7237a5775a8b8bd2edd4d
agasi <agarahimovagasi2@gmail.com>
2025-07-08 10:57:41 +0400

    Add greeting method to Vagif class

--- src/main/java/com/example/demo/Vagif.java
+++ src/main/java/com/example/demo/Vagif.java
@@ -2,4 +2,14 @@ package com.example.demo;
 
 public class Vagif {
 
+    private String name;
+
+    public Vagif(String name) {
+        this.name = name;
+    }
+
+    public void greet() {
+        System.out.println("Hello, my name is " + name);
+    }
+
 }
